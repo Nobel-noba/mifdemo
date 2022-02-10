a14ee25e7838a489339c7ac3853bd3c519090d6d
nobel-noba <nobelmitiku@gmail.com>
2022-02-10 14:33:27 +0300

    initial commit

---
+++ Mifdemo.Infrastructure/Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Mifdemo.Infrastructure.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions<Context> options) : base(options) { }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+           base.OnModelCreating(modelBuilder);          
+        }
+    }
+}
